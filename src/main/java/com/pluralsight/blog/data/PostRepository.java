adf82aa24e162b4db46bb08b48ccb46436dbb4e7
ahmet karakoc <akarakoc2@sgk.gov.tr>
2022-10-17 15:37:49 +0300

    odev1-1

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,3 +1,6 @@
 package com.pluralsight.blog.data;
 
+import com.pluralsight.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface PostRepository extends JpaRepository<Post, Long> {}
